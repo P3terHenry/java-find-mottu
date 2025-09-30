6174bfea17f390a7f33b170dbdf9c90f27ae9d61
P3terHenry <pedro.antonieti@outlook.com>
2025-09-30 12:33:47 -0300

    :wrench: Atualizado scripts SQL para remover e criar tabelas com suporte a SQL Server

--- src/main/resources/db/migration/V1__removendo_tabelas.sql
+++ src/main/resources/db/migration/V1__removendo_tabelas.sql
@@ -1,6 +1,24 @@
-drop table if exists t_mottu_cargo cascade;
-drop table if exists t_mottu_filiais cascade;
-drop table if exists t_mottu_localizacoes cascade;
-drop table if exists t_mottu_motos cascade;
-drop table if exists t_mottu_usuario cascade;
-drop table if exists t_usuario_cargo cascade;
+-- Remover foreign keys primeiro (SQL Server não suporta CASCADE em DROP TABLE)
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FKrm0ahqo3n4u1ydsfuhfibdus6')
+    ALTER TABLE t_mottu_localizacoes DROP CONSTRAINT FKrm0ahqo3n4u1ydsfuhfibdus6;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK8j4l0jqc2bjcmqnfq96kqbqki')
+    ALTER TABLE t_mottu_motos DROP CONSTRAINT FK8j4l0jqc2bjcmqnfq96kqbqki;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FK5ufk2na1t0i2w7rbc74dfd86h')
+    ALTER TABLE t_mottu_usuario DROP CONSTRAINT FK5ufk2na1t0i2w7rbc74dfd86h;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FKlkes5c85s3dn1ylel4sj2oga8')
+    ALTER TABLE t_usuario_cargo DROP CONSTRAINT FKlkes5c85s3dn1ylel4sj2oga8;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'FKtijnqqgyv757onv8hdqlq0mbf')
+    ALTER TABLE t_usuario_cargo DROP CONSTRAINT FKtijnqqgyv757onv8hdqlq0mbf;
+
+-- Agora dropar as tabelas
+DROP TABLE IF EXISTS t_usuario_cargo;
+DROP TABLE IF EXISTS t_mottu_localizacoes;
+DROP TABLE IF EXISTS t_mottu_motos;
+DROP TABLE IF EXISTS t_mottu_usuario;
+DROP TABLE IF EXISTS t_mottu_cargo;
+DROP TABLE IF EXISTS t_mottu_filiais;
+
--- src/main/resources/db/migration/V3__alterando_tabelas.sql
+++ src/main/resources/db/migration/V3__alterando_tabelas.sql
@@ -1,5 +1,16 @@
-alter table if exists t_mottu_localizacoes add constraint FKrm0ahqo3n4u1ydsfuhfibdus6 foreign key (id_moto) references t_mottu_motos;
-alter table if exists t_mottu_motos add constraint FK8j4l0jqc2bjcmqnfq96kqbqki foreign key (id_filial) references t_mottu_filiais;
-alter table if exists t_mottu_usuario add constraint FK5ufk2na1t0i2w7rbc74dfd86h foreign key (id_filial) references t_mottu_filiais;
-alter table if exists t_usuario_cargo add constraint FKlkes5c85s3dn1ylel4sj2oga8 foreign key (id_cargo) references t_mottu_cargo;
-alter table if exists t_usuario_cargo add constraint FKtijnqqgyv757onv8hdqlq0mbf foreign key (id_usuario) references t_mottu_usuario;
+-- Adicionar foreign keys (SQL Server não suporta IF EXISTS em ALTER TABLE)
+IF OBJECT_ID('t_mottu_localizacoes', 'U') IS NOT NULL
+    ALTER TABLE t_mottu_localizacoes ADD CONSTRAINT FKrm0ahqo3n4u1ydsfuhfibdus6 FOREIGN KEY (id_moto) REFERENCES t_mottu_motos(id_moto);
+
+IF OBJECT_ID('t_mottu_motos', 'U') IS NOT NULL
+    ALTER TABLE t_mottu_motos ADD CONSTRAINT FK8j4l0jqc2bjcmqnfq96kqbqki FOREIGN KEY (id_filial) REFERENCES t_mottu_filiais(id_filial);
+
+IF OBJECT_ID('t_mottu_usuario', 'U') IS NOT NULL
+    ALTER TABLE t_mottu_usuario ADD CONSTRAINT FK5ufk2na1t0i2w7rbc74dfd86h FOREIGN KEY (id_filial) REFERENCES t_mottu_filiais(id_filial);
+
+IF OBJECT_ID('t_usuario_cargo', 'U') IS NOT NULL
+    ALTER TABLE t_usuario_cargo ADD CONSTRAINT FKlkes5c85s3dn1ylel4sj2oga8 FOREIGN KEY (id_cargo) REFERENCES t_mottu_cargo(id_cargo);
+
+IF OBJECT_ID('t_usuario_cargo', 'U') IS NOT NULL
+    ALTER TABLE t_usuario_cargo ADD CONSTRAINT FKtijnqqgyv757onv8hdqlq0mbf FOREIGN KEY (id_usuario) REFERENCES t_mottu_usuario(id_usuario);
+
