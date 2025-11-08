5eb705d53e41e0bc80e233a68e033bd9c59f514d
P3terHenry <pedro.antonieti@outlook.com>
2025-11-08 03:15:34 -0300

    :sparkles: Adiciona repositório Arquivo com método de busca por squareId

---
+++ src/main/java/br/com/fiap/find_mottu/repository/ArquivoRepository.java
@@ -0,0 +1,13 @@
+package br.com.fiap.find_mottu.repository;
+
+import br.com.fiap.find_mottu.model.Arquivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArquivoRepository extends JpaRepository<Arquivo, Long> {
+
+    Optional<Arquivo> findBySquareId(String squareId);
+}
